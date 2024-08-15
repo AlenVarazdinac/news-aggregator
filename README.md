63df7ce248dc7dcb706f622db44271f0a0c9461d
Alen <alen.varazdinac@gmail.com>
2024-08-15 18:19:24 +0200

    News fetch and filters style

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+
+1. https://developer.nytimes.com/ https://developer.nytimes.com/my-apps/0f48c9d9-e9e2-4329-b6ff-f805d2b13cfc
+2. https://newsapi.org/ https://newsapi.org/docs/endpoints/everything
+3. https://open-platform.theguardian.com/documentation/ https://open-platform.theguardian.com/documentation/
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,193 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react"
+import axios from "axios"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import "./App.css"
+
+function debounce(func, wait) {
+  let timeout
+  return function (...args) {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func.apply(this, args), wait)
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [keywordTerm, setKeywordTerm] = useState('')
+  const [articles, setArticles] = useState([])
+  const [dateValue, setDateValue] = useState(7)
+
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(null)
+  const onDatePickerChange = (dates) => {
+    const [start, end] = dates
+    console.log("startDate", startDate)
+    console.log("endDate", endDate)
+    setStartDate(start)
+    setEndDate(end)
+  }
+
+  const NEWSAPI_KEY = import.meta.env.VITE_NEWSAPI_KEY
+  const GUARDIAN_API_KEY = import.meta.env.VITE_GUARDIAN_API_KEY
+  const NY_API_KEY = import.meta.env.VITE_NY_TIMES_API_KEY
+
+  const fetchArticles = async (keyword = '') => {
+    console.log('fetching articles...')
+    // axios
+    //   .get("https://newsapi.org/v2/top-headlines", {
+    //     headers: {
+    //       "X-Api-Key": NEWSAPI_KEY,
+    //     },
+    //     params: {
+    //       q: keyword,
+    //       from: "2024-07-15",
+    //       to: "2024-08-15",
+    //       pageSize: 20,
+    //       page: 1,
+    //       language: "en",
+    //       sortBy: "publishedAt",
+    //       category: "business",
+    //     },
+    //   })
+    //   .then((res) => {
+    //     if (!res.data.articles) {
+    //       console.log("Something went wrong.")
+    //       return
+    //     }
+    //     let articles = []
+    //     res.data.articles.map((article) => {
+    //       if (article.title === "[Removed]") return
+    //       articles.push({
+    //         title: article.title,
+    //         url: article.url,
+    //         image: article.urlToImage,
+    //         publishedAt: article.publishedAt,
+    //         source: article.source.name,
+    //       })
+    //     })
+
+    //     console.log("NewsApi", res.data)
+    //     console.log("articles", articles)
+    //     setArticles(articles)
+    //   })
+
+    // axios
+    //   .get("https://content.guardianapis.com/search", {
+    //     params: {
+    //       q: keyword,
+    //       "api-key": GUARDIAN_API_KEY,
+    //       "page-size": 20,
+    //       pages: 1,
+    //       "from-date": "2024-07-15",
+    //       "to-date": "2024-08-15",
+    //       tag: "business/business",
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log("GuardianApi", res)
+    //   })
+
+    // axios
+    //   .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+    //     params: {
+    //       q: keyword,
+    //       "api-key": NY_API_KEY,
+    //       page: 1,
+    //       begin_date: "2024-07-15",
+    //       end_date: "2024-08-15",
+    //       sort: "newest",
+    //       fq: 'news_desk:("Business")',
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log("NYApi", res)
+    //   })
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+  const articleObject = {
+    title: "",
+    description: "???",
+    url: "",
+    image: "",
+    publishedAt: "",
+    source: "",
+  }
+
+  const debouncedFetchArticles = useCallback(debounce(keyword => fetchArticles(keyword), 500), [])
+
+
+  const handleKeywordChange = (event) => {
+    const value = event.target.value
+    console.log('value', value)
+    setKeywordTerm(value)
+    debouncedFetchArticles(value)
+  }
+
+  const handleDateChange = (event) => {
+    console.log("date change", event.target.value)
+    setDateValue(event.target.value)
+  }
+
+  const handleSourceChange = (event) => {
+    console.log("source change", event.target.value)
+  }
+
+  const handleCategoryChange = (event) => {
+    console.log("category change", event.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="filter">
+        <input
+          type="text"
+          value={keywordTerm}
+          onChange={handleKeywordChange}
+          placeholder="Search for articles..."
+        />
+
+        <select name="date" onChange={handleDateChange}>
+          <option value="7">Last 7 Days</option>
+          <option value="14">Last 14 Days</option>
+          <option value="30">Last 30 Days</option>
+          <option value="custom">Custom</option>
+        </select>
+
+        {dateValue === "custom" && (
+          <DatePicker
+            selected={startDate}
+            onChange={onDatePickerChange}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+          />
+        )}
+
+        <select name="source" onChange={handleSourceChange}>
+          <option value="News API">News API</option>
+          <option value="newyorktimes">New York Times</option>
+          <option value="theguardian">The Guardian</option>
+        </select>
+
+        <select name="category" onChange={handleCategoryChange}>
+          <option value="all">All</option>
+          <option value="cars">Cars</option>
+          <option value="business">Business</option>
+          <option value="food">Food</option>
+          <option value="health">Health</option>
+          <option value="science">Science</option>
+          <option value="sports">Sports</option>
+          <option value="technology">Technology</option>
+        </select>
+
+        {articles.map((article, index) => (
+          <p key={index}>{article.title}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
